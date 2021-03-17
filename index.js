29959eeceb09e18618553efd974804c4b647b473
Jon <jbrundagejr@gmail.com>
2021-03-17 13:43:50 -0400

    commit tree 1563ff1f324693cc0ce0e4f7e2dfb7fc3febde14

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your solution in this file!
+customerName = "bob"
+function upperCaseCustomerName(customerName) {
+    console.log(customerName.toUpperCase());
+}
+function setBestCustomer() {
+    var bestCustomer = "not bob"
+    console.log(bestCustomer)
+}
